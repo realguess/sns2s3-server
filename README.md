cc87482f0f153a33f53f834f4c82b770490b7ab7
Chao Huang <chao@realguess.net>
2015-03-10 06:13:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SNS2S3 Server
+
+Place Amazon SNS messages into Amazon S3 bucket.
+
+
+## Usage
+
+Obtain [crendentials from environment variables][1]:
+
+```
+$ AWS_ACCESS_KEY_ID=akid AWS_SECRET_ACCESS_KEY=secret node server
+```
+
+[1]: http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html#Credentials_from_Environment_Variables
---
+++ app.js
@@ -0,0 +1,18 @@
+// Put Amazon SNS messages into Amazon S3 bucket.
+
+'use strict';
+
+var aws = require('aws-sdk');
+var server = require('realguess/sns-server');
+var s3 = new aws.S3();
+var port = process.env.PORT || 3000;
+var bucket = process.env.S3_BUCKET;
+var options = {
+  notify: function (data, done) {
+    var key = new Date().toISOString();
+
+    s3.putObject({ Bucket: bucket, Key: key, Body: data }, done);
+  }
+};
+
+server(options).listen(port);
